1664e6ababf7a469dca83dc6783432c14142f9f5
Aaryan Moorthy <63792258+AaryanMoorthy@users.noreply.github.com>
2020-05-21 21:30:10 +0530

    The code for Plot 04

--- Plot04.R
+++ Plot04.R
@@ -1,15 +1,16 @@
+
 dataFile <- "household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+subsetDataSet <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
-str(subSetData)
-datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-globalActivePower <- as.numeric(subSetData$Global_active_power)
-globalReactivePower <- as.numeric(subSetData$Global_reactive_power)
-voltage <- as.numeric(subSetData$Voltage)
-subMetering1 <- as.numeric(subSetData$Sub_metering_1)
-subMetering2 <- as.numeric(subSetData$Sub_metering_2)
-subMetering3 <- as.numeric(subSetData$Sub_metering_3)
+str(subsetDataSet)
+datetime <- strptime(paste(subsetDataSet$Date, subsetDataSet$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subsetDataSet$Global_active_power)
+globalReactivePower <- as.numeric(subsetDataSet$Global_reactive_power)
+voltage <- as.numeric(subsetDataSet$Voltage)
+subMetering1 <- as.numeric(subsetDataSet$Sub_metering_1)
+subMetering2 <- as.numeric(subsetDataSet$Sub_metering_2)
+subMetering3 <- as.numeric(subsetDataSet$Sub_metering_3)
 
 
 png("plot4.png", width=480, height=480)
